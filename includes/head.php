52605c84233b71dd9f46c0f148610cdb90b73cd7
satybaldy askhat <satybaldy.askhat@mail.ru>
2020-11-06 16:40:52 +0600

    askhat

---
+++ includes/head.php
@@ -0,0 +1,16 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="https://preview.colorlib.com/theme/eden/img/xfavicon.png.pagespeed.ic.66Q9flSNoC.webp"
+        type="image/png">
+    <title>Eden Megazine</title>
+    <!-- <base href="https://preview.colorlib.com/theme/eden/"> -->
+    <!-- Bootstrap CSS -->
+    <!-- main css -->
+    <link rel="stylesheet" href="/css/bootstrap.css">
+
+    <link rel="stylesheet" href="/css/font.css">
+    <link rel="stylesheet" href="/css/style.css">
+
+</head>
---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<header id="header" class="header_area">
+        <div class="main_menu">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
+                            <li class="nav-item active"><a class="nav-link" href="index.html">Домашняя</a></li>
+                            <li class="nav-item"><a class="nav-link" href="category.html">Категории</a></li>
+                            <li class="nav-item"><a class="nav-link" href="archive.html">Архив</a></li>
+                            <li class="nav-item submenu dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+                                    aria-haspopup="true" aria-expanded="false">Блог</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item"><a class="nav-link" href="blog.html">Блог</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="single-blog.html">Подробности блога</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="blog.html">Последние новости</a></li>
+                            <li class="nav-item"><a class="nav-link" href="contact.html">Свяжитесь с нами</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
