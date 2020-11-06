e6363088593de4e2264b346eb565fac7593ef1d6
11 <71764207+Yerezhep@users.noreply.github.com>
2020-11-06 16:22:41 +0600

    add includes files

--- blog.php
+++ blog.php
@@ -1,22 +1,7 @@
 <!doctype html>
 <html lang="en">
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="https://preview.colorlib.com/theme/eden/img/xfavicon.png.pagespeed.ic.66Q9flSNoC.webp"
-        type="image/png">
-    <title>Eden news</title>
-    <!-- <base href="https://preview.colorlib.com/theme/eden/"> -->
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet"
-        href="A.css,,_bootstrap.css+css,,_themify-icons.css+css,,_flaticon.css+vendors,,_fontawesome,,_css,,_all.min.css+vendors,,_animate-css,,_animate.css+vendors,,_popup,,_magnific-popup.css,Mcc.3vX99CUlJO.css.pagespeed.cf.NJGLkYXt6P.css" />
-    <!-- main css -->
-    <link rel="stylesheet" href="/css/bootstrap.css">
-    <link rel="stylesheet" href="/css/font.css">
-    <link rel="stylesheet" href="/css/style.css">
-</head>
+<?php include "includes/head.php"; ?>
 
 <body>
     <!--================ Start header Top Area =================-->
@@ -250,40 +235,7 @@
     </section>
     <!--================ End header top Area =================-->
     <!-- Start header Menu Area -->
-    <header id="header" class="header_area">
-        <div class="main_menu">
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="container">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-                        <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
-                            <li class="nav-item active"><a class="nav-link" href="index.html">Домашняя</a></li>
-                            <li class="nav-item"><a class="nav-link" href="category.html">Категории</a></li>
-                            <li class="nav-item"><a class="nav-link" href="archive.html">Архив</a></li>
-                            <li class="nav-item submenu dropdown">
-                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
-                                    aria-haspopup="true" aria-expanded="false">Блог</a>
-                                <ul class="dropdown-menu">
-                                    <li class="nav-item"><a class="nav-link" href="blog.html">Блог</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="single-blog.html">Подробности блога</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="nav-item"><a class="nav-link" href="blog.html">Последние новости</a></li>
-                            <li class="nav-item"><a class="nav-link" href="contact.html">Свяжитесь с нами</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
+    <?php include "includes/header.php"; ?>
     <!-- End header MEnu Area -->
     <!--================Home Banner Area =================-->
     <section class="banner_area">
@@ -527,134 +479,11 @@
     </section>
     <!-- ================ Instargram Area End ================= -->
     <!-- ================ start footer Area ================= -->
-    <footer class="footer-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
-                    <h4>Насчет нас</h4>
-                    <p>Небеса плодородны, но в меньшие дни не появляются ползучие сезоны, так что вот дни плодородия открыты</p>
-                    <div class="footer-logo">
-                        <img src="data:image/webp;base64,UklGRsQBAABXRUJQVlA4TLcBAAAvcEAJEPdAkG1TntPcX2gKAoEknv1V9hEIpDiefRYIEBb8R0qQYDWKPzAgS7KttoEuwZKtBIpOiP3vU+89wuT5iuj/BKByT+IX+o6k/91q7C2v+3k5lft/pXFKxc/Pocp/On5eltlnc9M0uWzTNLl8XS93En+ESIYDr7TOKtJ0AjdeHTBYddVdrtROcETpGVLuSjxQTpE9rOKNq6Hn3jErY1TiXXY3SmpqOmIJo5I7UWpfzxxLNCqjy9fU8+G+27p/EwbFPsbOMLcUbTlTQUf4yO64LjJDWVxbSjlZBzhL6Sxt4kiszA56lUyKBjkwnagHgKGEQ86dCuBPQU89AEytVmwjsbi62t7UK0tL9VOrlVRL+h9qy9JQibfeSdwP9a6jGUAyGj/GQYUsD5luR/A/2EbFJ7NGQS/Q7QhxGe9FOtkA8Ew8iDVKHNcDgGuHGpYzxuAF6RoAAhPPYztCFMEy6tZ1N6VUBT5ez1Irl3oZObYt9yLiVgjfAmhT1FTsXYkvAZtiir2otRT8KQoJaBK0K+WJE8WAgwsbQgKmRmAH0AUwhxg/HlUux+v13GbkHbt7e38MDhUDAA=="
-                            alt="" data-pagespeed-url-hash="2634304158"
-                            onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
-                    <h4>Контактная информация</h4>
-                    <div class="footer-address">
-                        <p>Адрес: Ваш адрес идет
-                            вот ваш демонстрационный адрес.</p>
-                        <span>Телефон: +8880 44338899</span>
-                        <span>Электронное письмо: info@colorlib.com</span>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
-                    <h4>Важная ссылка</h4>
-                    <ul>
-                        <li><a href="#">WHMCS-мост</a></li>
-                        <li><a href="#">Поиск домена</a></li>
-                        <li><a href="#">Мой аккаунт</a></li>
-                        <li><a href="#">Корзина покупателя</a></li>
-                        <li><a href="#">Наш магазин</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-sm-6 col-md-6 mb-4 mb-xl-0 single-footer-widget">
-                    <h4>Новостная рассылка</h4>
-                    <p>Небеса плодоносят не меньше дней. Появляются ползучие сезоны, вот дни рождения
-                        открытым</p>
-                    <div class="form-wrap" id="mc_embed_signup">
-                        <form target="_blank"
-                            action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01">
-                            <div class="input-group">
-                                <input type="email" class="form-control" name="EMAIL" placeholder="Ваш адрес электронной почты"
-                                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ваш адрес электронной почты '">
-                                <div class="input-group-append">
-                                    <button class="btn click-btn" type="submit">
-                                        <i class="fab fa-telegram-plane"></i>
-                                    </button>
-                                </div>
-                            </div>
-                            <div style="position: absolute; left: -5000px;">
-                                <input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-                            </div>
-                            <div class="info"></div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-bottom row align-items-center text-center text-lg-left no-gutters">
-                <p class="footer-text m-0 col-lg-8 col-md-12">
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Авторские права &copy;<script>
-                        document.write(new Date().getFullYear());
-                    </script> Все права защищены | Этот шаблон сделан с <i class="fa fa-heart"
-                        aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                </p>
-                <div class="col-lg-4 col-md-12 text-center text-lg-right footer-social">
-                    <a href="#"><i class="ti-facebook"></i></a>
-                    <a href="#"><i class="ti-twitter-alt"></i></a>
-                    <a href="#"><i class="ti-dribbble"></i></a>
-                    <a href="#"><i class="ti-linkedin"></i></a>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php include "includes/footer.php"; ?>
     <!-- ================ End footer Area ================= -->
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="/js/jquery-2.2.4.min.js.pagespeed.jm.pJg2D_p7ze.js"></script>
-    <script src="/js/popper.js+bootstrap.min.js+stellar.js.pagespeed.jc.dNpLUAstVq.js"></script>
-    <script>
-        eval(mod_pagespeed_qDngLsykIz);
-    </script>
-    <script>
-        eval(mod_pagespeed_OQW4QBllrd);
-    </script>
-    <script>
-        eval(mod_pagespeed_rI2L2pOfIy);
-    </script>
-    <script
-        src="/js/vendors,_popup,_jquery.magnific-popup.min.js+js,_jquery.ajaxchimp.min.js+js,_waypoints.min.js+js,_mail-script.js+js,_contact.js+js,_jquery.form.js.pagespeed.jc.EvdpWaJNJZ.js">
-    </script>
-    <script>
-        eval(mod_pagespeed_DvpK6qmaMC);
-    </script>
-    <script>
-        eval(mod_pagespeed_rApYH7FmlC);
-    </script>
-    <script>
-        eval(mod_pagespeed_ElEBEi0Ajo);
-    </script>
-    <script>
-        eval(mod_pagespeed_f9xQRUz2Fa);
-    </script>
-    <script>
-        eval(mod_pagespeed_3YXsAvJxlq);
-    </script>
-    <script>
-        eval(mod_pagespeed_FhvypHGIGb);
-    </script>
-    <script src="/js/jquery.validate.min.js+mail-script.js+theme.js.pagespeed.jc.M0GXsTotjZ.js"></script>
-    <script>
-        eval(mod_pagespeed_wtamv5rJxy);
-    </script>
-    <script>
-        eval(mod_pagespeed_f9xQRUz2Fa);
-    </script>
-    <script>
-        eval(mod_pagespeed_43YZNSQKOx);
-    </script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config', 'UA-23581568-13');
-    </script>
+    <?php include "includes/js.php"; ?>
 </body>
 
 </html>
---
+++ includes/footer.php
@@ -0,0 +1,73 @@
+<footer class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+                    <h4>Про нас</h4>
+                    <p>Небеса плодородны, но в меньшие дни не появляются ползучие сезоны, так что вот дни плодородия открыты</p>
+                    <div class="footer-logo">
+                        <img src="data:image/webp;base64,UklGRsQBAABXRUJQVlA4TLcBAAAvcEAJEPdAkG1TntPcX2gKAoEknv1V9hEIpDiefRYIEBb8R0qQYDWKPzAgS7KttoEuwZKtBIpOiP3vU+89wuT5iuj/BKByT+IX+o6k/91q7C2v+3k5lft/pXFKxc/Pocp/On5eltlnc9M0uWzTNLl8XS93En+ESIYDr7TOKtJ0AjdeHTBYddVdrtROcETpGVLuSjxQTpE9rOKNq6Hn3jErY1TiXXY3SmpqOmIJo5I7UWpfzxxLNCqjy9fU8+G+27p/EwbFPsbOMLcUbTlTQUf4yO64LjJDWVxbSjlZBzhL6Sxt4kiszA56lUyKBjkwnagHgKGEQ86dCuBPQU89AEytVmwjsbi62t7UK0tL9VOrlVRL+h9qy9JQibfeSdwP9a6jGUAyGj/GQYUsD5luR/A/2EbFJ7NGQS/Q7QhxGe9FOtkA8Ew8iDVKHNcDgGuHGpYzxuAF6RoAAhPPYztCFMEy6tZ1N6VUBT5ez1Irl3oZObYt9yLiVgjfAmhT1FTsXYkvAZtiir2otRT8KQoJaBK0K+WJE8WAgwsbQgKmRmAH0AUwhxg/HlUux+v13GbkHbt7e38MDhUDAA=="
+                            alt="" data-pagespeed-url-hash="2634304158"
+                            onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+                    <h4>Контактная информация</h4>
+                    <div class="footer-address">
+                        <p>Адрес: Ваш адрес идет
+                            вот ваш демонстрационный адрес.</p>
+                        <span>Телефон: +8880 44338899</span>
+                        <span>Электроннпя почта: info@colorlib.com</span>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+                    <h4>Важная ссылка</h4>
+                    <ul>
+                        <li><a href="#">WHMCS-мост</a></li>
+                        <li><a href="#">Поиск домена</a></li>
+                        <li><a href="#">Мой аккаунт</a></li>
+                        <li><a href="#">Корзина покупателя</a></li>
+                        <li><a href="#">Наш магазин</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-sm-6 col-md-6 mb-4 mb-xl-0 single-footer-widget">
+                    <h4>Новостная рассылка</h4>
+                    <p>Небеса плодоносят не меньше дней. Появляются ползучие сезоны, вот дни рождения
+                        открытым</p>
+                    <div class="form-wrap" id="mc_embed_signup">
+                        <form target="_blank"
+                            action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01">
+                            <div class="input-group">
+                                <input type="email" class="form-control" name="EMAIL" placeholder="Your Email Address"
+                                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Email Address '">
+                                <div class="input-group-append">
+                                    <button class="btn click-btn" type="submit">
+                                        <i class="fab fa-telegram-plane"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div style="position: absolute; left: -5000px;">
+                                <input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
+                            </div>
+                            <div class="info"></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom row align-items-center text-center text-lg-left no-gutters">
+                <p class="footer-text m-0 col-lg-8 col-md-12">
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Авторские права &copy;<script>
+                        document.write(new Date().getFullYear());
+                    </script>Все права защищены | Этот шаблон сделан с<i class="fa fa-heart"
+                        aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">colorlib</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+                <div class="col-lg-4 col-md-12 text-center text-lg-right footer-social">
+                    <a href="#"><i class="ti-facebook"></i></a>
+                    <a href="#"><i class="ti-twitter-alt"></i></a>
+                    <a href="#"><i class="ti-dribbble"></i></a>
+                    <a href="#"><i class="ti-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
