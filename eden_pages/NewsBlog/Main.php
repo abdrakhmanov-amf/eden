f03a294c2717223ab644cd4545c3f8d47ff4258f
abdrakhmanov-amf <misha2005a@icloud.com>
2020-11-13 16:28:09 +0600

    news to single block

--- eden_pages/NewsBlog/Main.php
+++ eden_pages/NewsBlog/Main.php
@@ -4,8 +4,8 @@ include "../core/news.php";
 
    $n1 = new News;
    $news_first = $n1->main(12,0,1);  // all data news
-   $query = DB::Query("SELECT * FROM `blog`");
-   $fetch = DB::Assoc($query);
+   
+   
 ?>
                 <div class="col-md-6 col-lg-6 col-xl-5">
                     <div class="single-blog">
@@ -17,7 +17,7 @@ include "../core/news.php";
                             <div class="meta-top d-flex">
                                 <a href="#"><?=$n1->get_category()?></a>
                             </div>
-                            <a class="d-block" href="eden_pages/single_blog.php?id=<?=$fetch['id']?>">
+                            <a class="d-block" href="eden_pages/single_blog.php?id=<?=$news_first['id']?>">
                                 <h4><?=$news_first['title']?></h4>
                             </a>
                             <div class="meta-bottom d-flex">
@@ -32,8 +32,8 @@ include "../core/news.php";
 <?php
    $n2 = new News;
    $news_second = $n2->main(1,0,1);  // all data news
-   $query = DB::Query("SELECT * FROM `blog`");
-   $fetch = DB::Assoc($query);
+
+
 ?>
                 <div class="col-md-6 col-lg-6 col-xl-4">
                     <div class="single-blog style_two">
@@ -45,7 +45,7 @@ include "../core/news.php";
                             <div class="meta-top d-flex justify-content-center">
                                 <a href="#"><?=$n2->get_category()?></a>
                             </div>
-                            <a class="d-block" href="eden_pages/single_blog.php?id=<?=$fetch['id']?>">
+                            <a class="d-block" href="eden_pages/single_blog.php?id=<?=$news_second['id']?>">
                                 <h4><?=$news_second['title']?></h4>
                             </a>
                             <div class="meta-bottom d-flex justify-content-center">
@@ -62,15 +62,15 @@ include "../core/news.php";
 
    $n4 = new News;
    $news_sport_2 = $n4->main(2,1,2);  // all data news
-   $query = DB::Query("SELECT * FROM `blog`");
-   $fetch = DB::Assoc($query);
+   
+   
 ?>
                 <div class="col-lg-12 col-xl-3">
                     <div class="row">
                         <div class="col-12 col-md-6 col-lg-6 col-xl-12">
                             <div class="single-blog style-three m_b_30">
                                 <div class="thumb">
-                                    <img class="img-fluid" src="/img/news/<?=$news_sport_1['img']?>" alt=""
+                                    <img class="img-fluid" src="/img/news/<?=$news_sport_2['img']?>" alt=""
                                         data-pagespeed-url-hash="350170539"
                                         >
                                 </div>
@@ -78,7 +78,7 @@ include "../core/news.php";
                                     <div class="meta-top d-flex justify-content-center">
                                         <a href="#"><?=$n3->get_category()?></a>
                                     </div>
-                                    <a class="d-block" href="eden_pages/single_blog.php?id=<?=$fetch['id']?>">
+                                    <a class="d-block" href="eden_pages/single_blog.php?id=<?=$news_sport_2['id']?>">
                                         <h4><?=$news_sport_1['title']?></h4>
                                     </a>
                                 </div>
